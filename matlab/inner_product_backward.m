ebfe1e56864ead98d3455f0362bc6d65c4ebae63
harkib <harkib@sfu.ca>
2020-09-18 14:58:45 -0700

    backprop functions

--- matlab/inner_product_backward.m
+++ matlab/inner_product_backward.m
@@ -3,4 +3,19 @@ function [param_grad, input_od] = inner_product_backward(output, input, layer, p
 % Replace the following lines with your implementation.
 param_grad.b = zeros(size(param.b));
 param_grad.w = zeros(size(param.w));
+input_od = zeros(size(input.data));
+
+batch_size = size(input,2);
+batch_len = size(input,1);
+
+
+for b = 1:batch_size
+
+    h_im1 = input.data(:,b);
+    param_grad.b = transpose(output.diff(:,b)) + param_grad.b;
+    param_grad.w = h_im1*transpose(output.diff(:,b)) + param_grad.w;
+    input_od(:,b) = param.w*output.diff(:,b);
+
+end
+
 end
--- matlab/relu_backward.m
+++ matlab/relu_backward.m
@@ -2,4 +2,18 @@ function [input_od] = relu_backward(output, input, layer)
 
 % Replace the following line with your implementation.
 input_od = zeros(size(input.data));
+batch_size = size(input,2);
+batch_len = size(input,1);
+
+
+for b = 1:batch_size
+    for i = 1:batch_len
+        h_im1 = input.data(i,b);
+        if h_im1 > 0
+            input_od(i,b) = output.diff(i,b);
+        else 
+            input_od(i,b) = 0;
+        end
+    end
+end
 end
